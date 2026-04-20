6891a833f37bdaebd3337f323a09b6bdfafa5366
Matheus Ribeiro Ávila <ribeiroavilamatheus@gmail.com>
2026-04-20 07:38:58 -0300

    `TASK-1` | Start project - First Commit

---
+++ src/test/java/com/crudoftasks/createTasks/CreateTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudoftasks.createTasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
